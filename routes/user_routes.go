edb081cbe7c1112e53d657626e471aa5dbda6b2f
allpyjs <allpyjs@gmail.com>
2025-03-17 10:53:43 -0700

    add Oauth with Google and JWT authentication

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -2,13 +2,22 @@ package routes
 
 import (
 	"golang-microservice/controllers"
+	"golang-microservice/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(router *gin.Engine) {
-	router.POST("/users", controllers.CreateUser)
-	router.GET("/users/:id", controllers.GetUser)
-	router.PUT("/users/:id", controllers.UpdateUser)
-	router.DELETE("/users/:id", controllers.DeleteUser)
+	secured := router.Group("/users").Use(middleware.AuthMiddleware())
+
+	secured.POST("", controllers.CreateUser)
+	secured.GET("/:id", controllers.GetUser)
+	secured.PUT("/:id", controllers.UpdateUser)
+	secured.DELETE("/:id", controllers.DeleteUser)
+}
+
+func AuthRoutes(router *gin.Engine) {
+	router.GET("/auth/login", middleware.OAuthLogin)
+	router.GET("/auth/callback", middleware.OAuthCallback)
+	router.POST("/login", controllers.Login)
 }
